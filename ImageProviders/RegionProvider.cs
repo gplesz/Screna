88399524d13dc7196b85e34b2e27f56784eceb69
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-04-12 07:54:29 +0530

    Added RegionProvider

---
+++ ImageProviders/RegionProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+
+namespace Screna
+{
+    public class RegionProvider : IImageProvider
+    {
+        readonly Rectangle _region;
+        readonly IOverlay[] _overlays;
+        
+        public RegionProvider(Rectangle Region, params IOverlay[] Overlays)
+        {
+            _region = Region;
+            _overlays = Overlays;
+        }
+
+        public Bitmap Capture()
+        {
+            var bmp = new Bitmap(_region.Width, _region.Height);
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(_region.Location,
+                                 Point.Empty,
+                                 _region.Size,
+                                 CopyPixelOperation.SourceCopy);
+
+                foreach (var overlay in _overlays)
+                    overlay.Draw(g, _region.Location);
+            }
+
+            return bmp;
+        }
+
+        public int Height => _region.Height;
+
+        public int Width => _region.Width;
+
+        public void Dispose()
+        {
+            foreach (var overlay in _overlays)
+                overlay.Dispose();
+        }
+    }
+}
